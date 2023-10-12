27e628dfaf295d1985542b7d86a8cf7d53659f92
leilapanova <144453728+leilapanova@users.noreply.github.com>
2023-10-12 20:00:05 +0200

    Add files via upload

---
+++ les2.py
@@ -0,0 +1,82 @@
+#task1
+# a = int(input())
+# b = int(input())
+# for i in range(a, b + 1):
+#     print(i)
+#task2
+# a = int(input())
+# b = int(input())
+# if b > a:
+#     for i in range(a, b+1):
+#         print(i)
+# else:
+#     for i in range(a, b-1, -1):
+#         print(i)
+#task3
+# sum = 0
+# n = int(input('n'))
+# for i in range(n):
+#     sum += int(input())
+#     print(sum)
+#task4
+# res = 1
+# n = int(input())
+# for i in range(1, n + 1):
+#     res *= i
+#     print(res)
+#task5
+# n = int(input('введите n: '))
+# if n < 10:
+#     for i in range(1, n + 1):
+#         print(str(i) * i)
+# else:
+#     print('число не в диапазоне от 1 до 9')
+#task6
+# n = int(input())
+# i = 1
+# while i**2 <= n:
+#     print(i, i**2)
+#     i += 1
+
+#task9
+# len = 0
+# while int(input()) != 0:
+#     len += 1
+#     print(len)
+#task7
+# n = int(input())
+# st = 0
+# k = 1
+# while k <= n//2:
+#     st += 1
+#     k *= 2
+#     print(st, k)
+#task8
+# x = int(input())
+# y = int(input())
+# d = 0
+# while x <= Y:
+#     X +=  0.1*x
+#     d += 1
+#     print(d)
+# #task9
+# x = int(input())
+# d = 0
+# while x !=0:
+#     x = int(input())
+#     d += 1
+#     print(d)
+#task12
+# n = int(input())
+# f0 = 0
+# f1 = 1
+# for i in range(n-1):
+#     f2 = f0  + f1
+#     f1, f0 = f2, f1
+#     print(f2)
+#task
+# list = [1, 2, 3, 4, 5, 6]
+# print(list[1::2])
+
+
+
